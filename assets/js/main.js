f9a30a21f1eca033af8d5b471322463a2aff126a
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2021-10-07 19:47:51 +0530

    recenty updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,15 +40,6 @@
     el.addEventListener('scroll', listener)
   }
 
-  // Preloader (if the #preloader div exists)
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
-  });
-
   /**
    * Navbar links active state on scroll
    */
