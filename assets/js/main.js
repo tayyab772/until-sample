848e434b51f0aae9fa4a9933d3312ebef3ad3a17
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-10 11:20:19 +0530

    removed the js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,27 +242,6 @@
 
 
 /**
- * Animation on scroll
- */
- function send_mail(){
-  document.querySelector('.loading').classList.add('display');
-
-  var temp = {
-      user_name: document.getElementById('name').value,
-      user_mail: document.getElementById('email').value,
-      msg: document.getElementById('message').value,
-  };
-
-  emailjs.send('service_ac-kln', 'template_ac-kln', temp)
-  .then(function(res){
-      document.getElementById("name").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("message").value = "";
-      document.querySelector('.loading').classList.remove('display');
-      document.querySelector('.sent-message').classList.add('display');
-      console.log("Success", res.status);
-  }).catch((error)=> {
-      this.querySelector('.loading').classList.remove('display');
-      this.querySelector('.error-message').classList.add('display');
-  });
-}
+ * Email send
+*/
+ 
