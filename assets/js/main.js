6f294ad8a95a10ac2370a8f6b967b4eea4b344ec
Vinojan Veerapathrathasan <vinojan02abhimanyu@gmail.com>
2022-09-01 13:10:31 +0530

    Added current changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,6 +242,19 @@
 
 
 /**
- * Email send
+ * NavBar
 */
- 
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+//Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("nav-fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
